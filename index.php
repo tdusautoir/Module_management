74d21a0c4e5785989c83965cfaa94dd911a0dc98
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-09-19 23:41:15 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module_manager</title>
+</head>
+
+<body>
+    <h1>Module Manager</h1>
+
+    <div class="content">
+        <form action="./add_module.php" method="POST">
+            <input type="name" name="name">
+            <button type="submit" name="submit">Ajouter le module</button>
+        </form>
+    </div>
+</body>
+
+</html>
