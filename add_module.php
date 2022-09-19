effa9477fae5b34fc045f9ff4e2cbfb6c60913cd
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-09-20 00:17:47 +0200

    add module

--- add_module.php
+++ add_module.php
@@ -1,3 +1,29 @@
 <?php
 
 require 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['submit'])) {
+        if (isset($_POST['name'])) {
+
+            $db->beginTransaction();
+
+            $module_name = $_POST['name'];
+
+            $stmt = $db->query("INSERT INTO module (name) VALUES ('$module_name');");
+            $stmt = $db->query("INSERT INTO history (id_module) VALUES (LAST_INSERT_ID());");
+
+            $db->commit();
+
+            if (!$stmt) {
+                print_r($db->errorInfo());
+            } else {
+                header('location: ./index.php');
+            }
+        }
+    } else {
+        return;
+    }
+} else {
+    return;
+}
--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $password = 'root';
 
 try {
     $db = new PDO("mysql:host=$hostname;dbname=$database;charset=UTF8", $username, $password);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage() . "<br/>";
     die();
